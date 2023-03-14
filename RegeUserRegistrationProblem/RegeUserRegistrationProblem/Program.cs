08952b8f4f0c773172b33a9242ef64ca94aac136
unknown <bhotnikhil25@gmail.com>
2023-03-15 00:37:23 +0530

    [Nikhil]-Validate First Name using Regex

---
+++ RegeUserRegistrationProblem/RegeUserRegistrationProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegeUserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                UC-01As a User need to enter a valid First Name
+                    - First name starts with Cap and has minimum 3 characters
+            */
+            Console.WriteLine("Enter The First Name Of User");
+            string first_Name = Console.ReadLine();
+            if (UserDetailCheck.ValidateFirstName(first_Name))
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+    }
+}
